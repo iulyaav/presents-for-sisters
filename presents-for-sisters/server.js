462b035af3933e1d60b21944b06a9e8a388d7d9a
Iulia Avram <avram.iulia.augusta@gmail.com>
2020-11-11 08:10:30 +0000

    Add some endpoints with no logic in them just to trigger a deploy on Heroku

--- presents-for-sisters/server.js
+++ presents-for-sisters/server.js
@@ -30,3 +30,15 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
 });
 
 // CONTACTS API ROUTES BELOW
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+
+
+app.get("/api/users", function(req, res) {
+});
+
+app.get("/api/users/:username", function(req, res) {
+});
